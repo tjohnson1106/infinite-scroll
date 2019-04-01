a3d40d5d18b6e0b09e99543436eb0f73268e47e9
Thomas Johnson <tjohnson1106@gmail.com>
2019-04-01 15:49:25 -0400

    created books map

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { List, ListItem, Paper } from "@material-ui/core";
+import { Waypoint } from "react-waypoint";
+
+import { useBooksQuery } from "../generated/ApolloHooks";
 
 const Books = () => {
-  // const { data } = useBooksQuery({
-  //     variables:
-  // })
+  const { data } = useBooksQuery({
+    variables: { first: 50 }
+  });
+
+  if (!data || !data.books) {
+    return <div> ...Loading </div>;
+  }
+
   return (
-    <div className="">
-      <h1>Books</h1>
+    <div className="books-root">
+      <div className="paper-wrapper">
+        <Paper>
+          <List>
+            {data.books.books.map((x) => (
+              <ListItem key={x.id}>{x.title}</ListItem>
+            ))}
+          </List>
+        </Paper>
+      </div>
     </div>
   );
 };
--- src/generated/ApolloHooks.tsx
+++ src/generated/ApolloHooks.tsx
@@ -1,77 +1,86 @@
-
 type Maybe<T> = T | null;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string,
-  String: string,
-  Boolean: boolean,
-  Int: number,
-  Float: number,
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
   /** The `Upload` scalar type represents a file upload. */
-  Upload: any,
+  Upload: any;
 };
 
-
 export type Book = {
-  id: Scalars['ID'],
-  title?: Maybe<Scalars['String']>,
-  author?: Maybe<Scalars['String']>,
+  id: Scalars["ID"];
+  title?: Maybe<Scalars["String"]>;
+  author?: Maybe<Scalars["String"]>;
 };
 
 export type BooksResponse = {
-  books: Array<Book>,
-  hasNextPage: Scalars['Boolean'],
+  books: Array<Book>;
+  hasNextPage: Scalars["Boolean"];
 };
 
 export enum CacheControlScope {
-  Public = 'PUBLIC',
-  Private = 'PRIVATE'
+  Public = "PUBLIC",
+  Private = "PRIVATE"
 }
 
 export type Mutation = {
-  removeBook: Scalars['Boolean'],
+  removeBook: Scalars["Boolean"];
 };
 
-
 export type MutationRemoveBookArgs = {
-  id: Scalars['String']
+  id: Scalars["String"];
 };
 
 export type Query = {
-  books: BooksResponse,
+  books: BooksResponse;
 };
 
-
 export type QueryBooksArgs = {
-  searchQuery?: Maybe<Scalars['String']>,
-  cursor?: Maybe<Scalars['String']>,
-  first: Scalars['Int']
+  searchQuery?: Maybe<Scalars["String"]>;
+  cursor?: Maybe<Scalars["String"]>;
+  first: Scalars["Int"];
 };
 
 export type BooksQueryVariables = {
-  cursor?: Maybe<Scalars['String']>,
-  first: Scalars['Int']
+  cursor?: Maybe<Scalars["String"]>;
+  first: Scalars["Int"];
 };
 
+export type BooksQuery = { __typename?: "Query" } & {
+  books: { __typename?: "BooksResponse" } & Pick<
+    BooksResponse,
+    "hasNextPage"
+  > & {
+      books: Array<
+        { __typename?: "Book" } & Pick<Book, "id" | "title" | "author">
+      >;
+    };
+};
 
-export type BooksQuery = ({ __typename?: 'Query' } & { books: ({ __typename?: 'BooksResponse' } & Pick<BooksResponse, 'hasNextPage'> & { books: Array<({ __typename?: 'Book' } & Pick<Book, 'id' | 'title' | 'author'>)> }) });
-
-import gql from 'graphql-tag';
-import * as ReactApolloHooks from 'react-apollo-hooks';
+import gql from "graphql-tag";
+import * as ReactApolloHooks from "react-apollo-hooks";
 
 export const BooksDocument = gql`
-    query Books($cursor: String, $first: Int!) {
-  books(cursor: $cursor, first: $first) {
-    books {
-      id
-      title
-      author
+  query Books($cursor: String, $first: Int!) {
+    books(cursor: $cursor, first: $first) {
+      books {
+        id
+        title
+        author
+      }
+      hasNextPage
     }
-    hasNextPage
   }
-}
-    `;
+`;
 
-export function useBooksQuery(baseOptions?: ReactApolloHooks.QueryHookOptions<BooksQueryVariables>) {
-  return ReactApolloHooks.useQuery<BooksQuery, BooksQueryVariables>(BooksDocument, baseOptions);
-};
+export function useBooksQuery(
+  baseOptions?: ReactApolloHooks.QueryHookOptions<BooksQueryVariables>
+) {
+  return ReactApolloHooks.useQuery<BooksQuery, BooksQueryVariables>(
+    BooksDocument,
+    baseOptions
+  );
+}
