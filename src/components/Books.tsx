ef88e9329fc282b4e93ccdba371bec81c74f8bcf
Thomas Johnson <tjohnson1106@gmail.com>
2019-04-01 15:04:51 -0400

    created books query

---
+++ src/components/Books.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { List, ListItem, Paper } from "@material-ui/core";
+
+const Books = () => {
+  // const { data } = useBooksQuery({
+  //     variables:
+  // })
+  return (
+    <div className="">
+      <h1>Books</h1>
+    </div>
+  );
+};
+
+export default Books;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo-hooks";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const client = new ApolloClient({
+  uri: "https://hurricane-frigate.glitch.me"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
